658213ae083bf65666684ded682456d185746fba
inyindjo1 <inyindjo1@gmail.com>
2025-05-21 17:33:09 -0400

    saving code

--- sba/DOM.js
+++ sba/DOM.js
@@ -4,49 +4,33 @@ const amountInput = document.getElementById('amount')
 const summaryList = document.querySelector('#donation-summary')
 const clearBtn = document.getElementById('clear-btn')
 
-console.log(clearBtn)
-let donation = localStorage.getItem('donation') || ''
-
-function updateSummary() {
-    summaryList.innerHTML = ''
-    for (let[charity,amount] of Object.entries(donation)){
-        const li = document.create[e]
-        li.textContent = `${charity}: ${amount}`
-        summaryList.appendChild(li)
-        console.log("First child text:", summaryList.firstChild.textContent)
-    }
-}
 
 form.addEventListener('submit',function (e) {
     e.preventDefault()
     const charity = charitySelect.value
     const amount = parseFloat( amountInput.value)
     let li = document.createElement("li")
-    li.textContent = amount
+    li.textContent = "$ "+amount
     summaryList.appendChild(li)
+    
     if (! charity || isNaN(amount) || amount <= 0){
         alert("please enter a valid charity and donation amount")
         return;
     }
-    // donations 
-    let donations = document.createElement("donations")
-    donations.textContent = amount;
-    (donation[charity] || 0) + amount;
-localStorage.setItem('donations', JSON.stringify(donations))
-
-// amount code part 
-alert(`Thank you for donating $${amount} to ${charity}!`)
-updateSummary()
-form.rest ()
+    
+alert(`Thank you for donating $${amount.toFixed(2)} to ${charity}!`)
+form.reset ()
 })
 clearBtn.addEventListener('click', function () {
     if (confirm("Are you sure you want to clear all donations?")) {
         donation = {}
-        localStorage.removeItem('donations');
-        updateSummary()
+        donation.length = 0
+        
     }
 });
 
-// Initial summary load
-updateSummary()
+
+
+
+
 
